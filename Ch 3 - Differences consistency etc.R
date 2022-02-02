68a001050721bb912f9d967ebbe2ae235db09110
Ruam Pimentel <ruampimentel@gmail.com>
2022-02-02 14:26:14 -0500

    alternative ways and files upload

--- Ch 3 - Differences consistency etc.R
+++ Ch 3 - Differences consistency etc.R
@@ -18,13 +18,23 @@ var(MRMTch3$MRS)
 sd(MRMTch3$MRS)
 #install.packages("sjstats")
 library(sjstats)
-var_pop(MRMTch3$MRS)
-sd_pop(MRMTch3$MRS)
+var_pop(MRMTch3$MRS) #same as: var(x) * ((n - 1)/n)
+sd_pop(MRMTch3$MRS) #sqrt of the previous output
+
 
 #3 Compute several descriptives one or more variables at a time
 #install.packages("psych")
 library(psych)
 describe(MRMTch3[,c("MRS", "MTS", "SWL")]) 
+
+MRMTch3 %>% 
+  select(MRS, MTS, SWL) %>% 
+  describe() %>% 
+  as_tibble(rownames = "variables") %>% 
+  select(variables, n:median, min:se) %>% 
+  arrange(kurtosis) %>% 
+  mutate(across(is.numeric, ~round(., 2)))
+  gt::gt()
 
 #4 Plot the shape/curve of a distribution
 hist(MRMTch3$MRS, 
@@ -33,15 +43,29 @@ hist(MRMTch3$MRS,
      xlab="MRS Score", 
      col="gray95")
 
+hist(MRMTch3$MTS, 
+     main ="Histogram of Moral Tolerance Scale Scores",
+     breaks=seq(1,5,by=.20), 
+     xlab="MTS Score", 
+     col="gray95")
+
 #5 Association between two variables - scatterplot
 ## make it a bit more pleasand
-plot(jitter(MRMTch3$MRS, 10), jitter(MRMTch3$MTS, 10), pch=20)
+plot(jitter(MRMTch3$MRS, 10), 
+     jitter(MRMTch3$MTS, 10), 
+     pch=20)
 ## no jitter
-plot(MRMTch3$MRS, MRMTch3$MTS, pch=20)
+plot(MRMTch3$MRS, 
+     MRMTch3$MTS, 
+     pch=20)
 
 #6 Association between two variables - covariance and correlation
 cov(MRMTch3$MRS, MRMTch3$MTS)
 cor(MRMTch3$MRS, MRMTch3$MTS)
+
+MRMTch3 %>% 
+  select(MRS, MTS) %>% 
+  cor()
 
 
 #7 Correlation matrix for multiple variables
